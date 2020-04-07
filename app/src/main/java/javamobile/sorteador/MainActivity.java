4136ad54ba22eeb3f08656ed1ff0bbf76a1d675d
Carlos Camara <augusto.rj.sp@gmail.com>
2020-04-07 17:14:21 -0300

    Commit final, foi adcionado uma funcionalidade no botão Sortear.

--- app/src/main/java/javamobile/sorteador/MainActivity.java
+++ app/src/main/java/javamobile/sorteador/MainActivity.java
@@ -1,7 +1,15 @@
 package javamobile.sorteador;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void selecionarBotao(View v) {
+        int num = new Random().nextInt(101);
+        TextView texto = findViewById(R.id.textoResultado);
+        texto.setText("O NUMERO SORTEADO FOI: " +num);
+
+    }
+
 }
+
+
+
